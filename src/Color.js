1afee459f4ed21dfe12e78b9a43f25074500a6f5
wuzhiqin <wuzhiqin@servasoft.com>
2019-07-15 17:11:07 +0800

    添加球形物体测试

--- src/Color.js
+++ src/Color.js
@@ -1,16 +1,30 @@
 import Vec3 from './Vec3.js';
 
 
+function hit_shpere(center, radius, ray) {
+  const oc = ray.getOrigin().clone().sub(center);
+  const a = ray.getDirection().dot(ray.getDirection());
+  const b = 2 * ray.getDirection().dot(oc);
+  const c = oc.dot(oc) - radius * radius;
+  const descriminant = b * b - 4 * a * c;
+  return descriminant > 0;
+}
+
 export default class Color extends Vec3 {
   constructor(ray) {
     super();
-    let rayDirection = ray.getDirection();
-    let unit = rayDirection.normalize();
 
-    let t = 0.5 * (unit.y + 1);
-    let v = new Vec3(1, 1, 1).multiplyScalar(1 - t).add(new Vec3(0.5, 0.7, 1).multiplyScalar(t));
-    this.x = v.x;
-    this.y = v.y;
-    this.z = v.z;
+    if (hit_shpere(new Vec3(0, 0, -1), 0.5, ray)) {
+      this.set(1, 0, 0);
+    } else {
+
+      let rayDirection = ray.getDirection();
+      let unit = rayDirection.normalize();
+
+      let t = 0.5 * (unit.y + 1);
+      let v = new Vec3(1, 1, 1).multiplyScalar(1 - t).add(new Vec3(0.5, 0.7, 1).multiplyScalar(t));
+      this.set(v.x, v.y, v.z);
+    }
+
   }
 }
